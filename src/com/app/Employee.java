1dc0afbb81ee9cf59796b6efd92697f15dee075e
kamal <kamal@DESKTOP-MDQO5N9>
2017-06-18 14:39:27 +0530

    + Added - Removed

--- src/com/app/Employee.java
+++ src/com/app/Employee.java
@@ -3,7 +3,7 @@ package com.app;
 public class Employee {
 
 	private int empId;
-	private String empName;
+	private double empSal;
 
 	public int getEmpId() {
 		return empId;
@@ -13,11 +13,11 @@ public class Employee {
 		this.empId = empId;
 	}
 
-	public String getEmpName() {
-		return empName;
+	public double getEmpSal() {
+		return empSal;
 	}
 
-	public void setEmpName(String empName) {
-		this.empName = empName;
+	public void setEmpSal(double empSal) {
+		this.empSal = empSal;
 	}
 }
